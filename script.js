14b43677c222db5ba2ec9c56f9c7597ddca1502a
dasensi007 <albuquerq1v@gmail.com>
2025-06-12 12:08:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function createHearts() {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = 3 + Math.random() * 2 + "s";
+  document.body.appendChild(heart);
+  setTimeout(() => heart.remove(), 5000);
+}
+
+setInterval(createHearts, 300);
